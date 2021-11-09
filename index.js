4c18b2f022a648a46b7cacd73273f15c7bf25e03
mec-nyan <marianoraspberrypi@gmail.com>
2021-11-08 21:04:36 -0300

    remove example files. move index to dist folder

--- index.js
+++
@@ -1,6 +0,0 @@
-// basic pong like screen in ts
-const root = document.getElementById('root');
-const title = 'This is Pong, this is Pong is TS';
-const h1 = document.createElement('h1');
-h1.innerText = title;
-root.appendChild(h1);
