c25804ecbd9afd313b742ba6a8139bdd76cec5f9
mec-nyan <marianoraspberrypi@gmail.com>
2021-11-08 21:02:58 -0300

    remove example files

--- hello.js
+++
@@ -1,8 +0,0 @@
-// Classic hello world example in JS
-console.log('Hello world');
-// annotations
-function greet(person, date) {
-    console.log(`Hello, ${person}! Today is ${date.toDateString()}.`);
-}
-greet('Mariano', new Date());
-let message = 'Hello, you ther';
